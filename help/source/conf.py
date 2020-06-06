5eb0812702313c55d99604262952f403698ed255
Martin Landa <landa.martin@gmail.com>
2020-06-06 19:37:08 +0200

    footer cosmentics

--- help/source/conf.py
+++ help/source/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'GTFS'
-copyright = u'2013, Skupina B'
+project = u'GTFS QGIS Plugin'
+copyright = u'2020, CTU GeoForAll Lab'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
